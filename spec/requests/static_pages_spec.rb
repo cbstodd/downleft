64fff10cca8c7f48dee5c4f2787d45c4ad463cb3
Colin Stodd <cbstodd@gmail.com>
2014-02-07 19:40:00 -0500

    Finished StaticPages, aded rspec info

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do 
+
+    it "should have the content 'Downleft'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', text: 'Downleft')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/home'
+      page.should have_selector('title', text: '| Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', text: 'Help')
+    end
+
+    it "should have right title" do 
+      visit '/static_pages/help'
+      page.should have_selector('title', text: '| Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', text: 'About')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/about'
+      page.should have_selector('title', text: '| About')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the right title" do 
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: '| Contact')
+    end
+  end  
+
+end
