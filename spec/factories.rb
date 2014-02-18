805f917c202306348997e090ea369f1c8502ff76
Colin Stodd <cbstodd@gmail.com>
2014-02-18 12:32:25 -0500

    Added signup page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do 
 	factory :user do 
-		name 				  "Colin Stodd"
-		email 				  "cbstodd@me.com"
-		password 			  "foobar"
+		name 				          "Colin Stodd"
+		email 				        "cbstodd@me.com"
+		password 			        "foobar"
 		password_confirmation "foobar"
 	end
 end
